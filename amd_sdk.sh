4bf66b77fb416f096ccbeb9b06d2e7d2229c1a7d
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2016-04-14 23:29:57 +0200

    script

--- amd_sdk.sh
+++ amd_sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Original script from https://github.com/gregvw/amd_sdk/
 # Original modification from https://gist.github.com/vchuravy/1faa40c2df51e3a9ee55#file-amd_sdk-sh
@@ -35,4 +35,12 @@ NONCE2=`echo $FORM | awk -F ${NONCE2_STRING} '{print $2}'`
 NONCE2=`echo $NONCE2 | awk -F'"' '{print $2}'`
 echo $NONCE2
 
-wget --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" -O AMD-SDK.tar.bz2;
+wget -c --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" -O AMD-SDK.tar.bz2;
+
+tar xjf AMD-SDK.tar.bz2
+
+mkdir AMD_SDK
+sh AMD*.sh --noexec --target AMD_SDK
+
+AMD_SDK/install.sh -s y -a y
+
